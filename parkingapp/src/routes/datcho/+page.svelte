8d48ff6d33d650bd5b7a282dac59c145789d1738
21522388 <21522388@gm.uit.edu.vn>
2023-10-18 19:19:13 +0700

    Startup

---
+++ parkingapp/src/routes/datcho/+page.svelte
@@ -0,0 +1,3 @@
+<div class="flex justify-center pt-40 pb-40">
+    <h1 class="h1 animate-spin">:(</h1>
+</div>
---
+++ parkingapp/src/routes/trogiup/+page.svelte
@@ -0,0 +1,60 @@
+<script lang='ts'>
+    import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="flex flex-1 md:2/3 xl:w-1/2 mx-auto my-16 justify-center bg-black/10 rounded">
+    <Accordion autocollapse>
+        <AccordionItem>
+            <svelte:fragment slot="summary">
+                <p class="font-bold">Câu hỏi 1?</p>
+            </svelte:fragment>
+            <svelte:fragment slot="content">
+                Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+            </svelte:fragment>
+        </AccordionItem>
+        <AccordionItem>
+            <svelte:fragment slot="summary">
+                <p class="font-bold">Câu hỏi 2?</p>
+            </svelte:fragment>
+            <svelte:fragment slot="content">
+                Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+            </svelte:fragment>
+        </AccordionItem>
+        <AccordionItem>
+            <svelte:fragment slot="summary">
+                <p class="font-bold">Câu hỏi 3?</p>
+            </svelte:fragment>
+            <svelte:fragment slot="content">
+                Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+            </svelte:fragment>
+        </AccordionItem>
+        <AccordionItem>
+            <svelte:fragment slot="summary">
+                <p class="font-bold">Câu hỏi 4?</p>
+            </svelte:fragment>
+            <svelte:fragment slot="content">
+                Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+                <br>Trả lời cho câu hỏi
+            </svelte:fragment>
+        </AccordionItem>
+    </Accordion>
+</div>
